b3126df02d14fa9de4b31c1075d423fd82f64413
Frank Hunleth <fhunleth@troodon-software.com>
2019-02-23 22:41:26 -0500

    Add notes

--- keytar.c
+++ keytar.c
@@ -4,11 +4,19 @@
 #include <stdlib.h>
 #include "hidapi.h"
 
-// Headers needed for sleeping.
 #ifdef _WIN32
 	#include <windows.h>
 #else
 	#include <unistd.h>
+#endif
+
+#ifdef __APPLE__
+#include <ApplicationServices/ApplicationServices.h>
+
+static void press_key(char key)
+{
+
+}
 #endif
 
 static unsigned char counter = 0;
@@ -128,125 +136,16 @@ int main(int argc, char* argv[])
 	UNREFERENCED_PARAMETER(argv);
 #endif
 
-
 	if (hid_init())
 		return -1;
 
-#if 0
-	struct hid_device_info *devs, *cur_dev;
-	devs = hid_enumerate(0x0, 0x0);
-	cur_dev = devs;
-	while (cur_dev) {
-		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
-		printf("\n");
-		printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-		printf("  Product:      %ls\n", cur_dev->product_string);
-		printf("  Release:      %hx\n", cur_dev->release_number);
-		printf("  Interface:    %d\n",  cur_dev->interface_number);
-		printf("\n");
-		cur_dev = cur_dev->next;
-	}
-	hid_free_enumeration(devs);
-#endif
-
-	// Set up the command buffer.
-	memset(buf,0x00,sizeof(buf));
-	buf[0] = 0x01;
-	buf[1] = 0x81;
-
-
-	// Open the device using the VID, PID,
-	// and optionally the Serial number.
-	////handle = hid_open(0x4d8, 0x3f, L"12345");
 	handle = hid_open(0x1bad, 0x3330, NULL);
 	if (!handle) {
 		printf("Can't find the keytar\n");
  		return 1;
 	}
 
-#if 0
-	// Read the Manufacturer String
-	wstr[0] = 0x0000;
-	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read manufacturer string\n");
-	printf("Manufacturer String: %ls\n", wstr);
 
-	// Read the Product String
-	wstr[0] = 0x0000;
-	res = hid_get_product_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read product string\n");
-	printf("Product String: %ls\n", wstr);
-	// Read the Serial Number String
-	wstr[0] = 0x0000;
-	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read serial number string\n");
-	printf("Serial Number String: (%d) %ls", wstr[0], wstr);
-	printf("\n");
-
-	// Read Indexed String 1
-	wstr[0] = 0x0000;
-	res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read indexed string 1\n");
-	printf("Indexed String 1: %ls\n", wstr);
-#endif
-	// Set the hid_read() function to be non-blocking.
-	//hid_set_nonblocking(handle, 1);
-
-	// Try to read from the device. There shoud be no
-	// data here, but execution should not block.
-	//res = hid_read(handle, buf, 17);
-#if 0
-	// Send a Feature Report to the device
-	buf[0] = 0x2;
-	buf[1] = 0xa0;
-	buf[2] = 0x0a;
-	buf[3] = 0x00;
-	buf[4] = 0x00;
-	res = hid_send_feature_report(handle, buf, 17);
-	if (res < 0) {
-		printf("Unable to send a feature report.\n");
-	}
-
-	memset(buf,0,sizeof(buf));
-
-	// Read a Feature Report from the device
-	buf[0] = 0x2;
-	res = hid_get_feature_report(handle, buf, sizeof(buf));
-	if (res < 0) {
-		printf("Unable to get a feature report.\n");
-		printf("%ls", hid_error(handle));
-	}
-	else {
-		// Print out the returned buffer.
-		printf("Feature Report\n   ");
-		for (i = 0; i < res; i++)
-			printf("%02hhx ", buf[i]);
-		printf("\n");
-	}
-#endif
-	memset(buf,0,sizeof(buf));
-#if 0
-	// Toggle LED (cmd 0x80). The first byte is the report number (0x1).
-	buf[0] = 0x1;
-	buf[1] = 0x80;
-	res = hid_write(handle, buf, 17);
-	if (res < 0) {
-		printf("Unable to write()\n");
-		printf("Error: %ls\n", hid_error(handle));
-	}
-
-
-	// Request state (cmd 0x81). The first byte is the report number (0x1).
-	buf[0] = 0x1;
-	buf[1] = 0x81;
-	hid_write(handle, buf, 17);
-	if (res < 0)
-		printf("Unable to write() (2)\n");
-#endif
 
         for (;;) {
 	// Read requested state. hid_read() has been set to be
@@ -268,13 +167,7 @@ int main(int argc, char* argv[])
 
 cleanup:
 	hid_close(handle);
-
-	/* Free static HIDAPI objects. */
 	hid_exit();
-
-#ifdef WIN32
-	system("pause");
-#endif
 
 	return 0;
 }
